2c24708fa5b1f265f9f95010b326e276c4dbae96
okoro <mi.fracri91@gmail.com>
2023-10-03 12:45:38 +0100

    added playRound function to compare and choice winner

--- script.js
+++ script.js
@@ -8,18 +8,48 @@ function getComputerChoice(params) {
     if (randomNum === 0){
         return "Rock";
     }else if (randomNum === 1){
-        return "Papper";
+        return "Paper";
     } else {
         return "Scissors";
     }
  }
 
- console.log(getComputerChoice());
+
+ const computerSelection = getComputerChoice().toLowerCase();
+
+ console.log(computerSelection);
+
+ // get game player choice section
+
+ const playerSelection = prompt("choice one: ").toLowerCase();
+
+ console.log(playerSelection);
 
 
- const playerInput = prompt("choice one: ").toLowerCase();
+ function playRound(playerSelection, computerSelection) {
+    //compare both side to deterrmine the game winner
+    if (playerSelection === computerSelection) {
+        return "Draw!!! no winner, try again"
+    }else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "you win the game! Rock beat Scissors";
+    }else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "you win the game! Paper beat Rock";
+    }else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "you win the game! Scissors beat Paper";
+        //player winn ssection ends here
+    }else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "you loss the game! Rock beat Scissors";
+    }else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "you loss the game! Paper beat Rock";
+    }else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "you loss the game! Scissors beat Paper";
+    }else {
+        return "Game Over";
+    }
+ }
 
- console.log(playerInput);
+ console.log(playRound(playerSelection, computerSelection));
 
- 
+
+
 
