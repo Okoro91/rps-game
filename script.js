0dd477701ea72b48f5d36169b8df15d9ebf433dc
okoro <mi.fracri91@gmail.com>
2023-10-03 10:28:32 +0100

    added getcomputerCoice funtion

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+function getComputerChoice(params) {
+    randomNum = Math.floor(Math.random() * 3);
+
+    if (randomNum === 0){
+        return "Rock";
+    }else if (randomNum === 1){
+        return "Papper";
+    } else {
+        return "Scissors";
+    }
+ }
+
+ console.log(getComputerChoice());
+
